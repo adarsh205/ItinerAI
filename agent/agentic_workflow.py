42eec1308b8b3a87ece006bce634015339c75bd1
adarsh205 <adarshg205205@gmail.com>
2025-08-20 16:19:16 +0530

    created langgraph workflow

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -11,13 +11,30 @@ from prompt_library.prompt import SYSTEM_PROMPT
 class GraphBuilder():
 
     def __init__(self):
-        pass
+        self.tools = [
+            WeatherInfoTool(),
+            CalculatorTool(),
+            PlaceSearchTool(),
+            CurrencyConversionTool(),
+        ]
 
-    def agent_function(self):
-        pass
+    def agent_function(self, state: MessagesState):
+        conversation = state['messages']
+        ip = [SYSTEM_PROMPT] + conversation
+        response = self.llm_with_tools.invoke(ip)
+        return {'messages': [response]}
 
     def build_graph(self):
-        pass
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge("tools", "agent")
+        graph_builder.add_edge("agent", END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
 
     def __call__(self):
-        pass
+        return self.build_graph()
