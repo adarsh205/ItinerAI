1dc69c84c1920635ad00dfd1451ce7f1c2a6e493
adarsh205 <adarshg205205@gmail.com>
2025-08-21 10:47:28 +0530

    updated workflow

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -2,7 +2,7 @@ from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from utils.model_loader import ModelLoader
 from tools.weather_info_tool import WeatherInfoTool
-from tools.calculator_tool import CalculatorTool
+from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConversionTool
 from tools.place_search_tool import PlaceSearchTool
 from prompt_library.prompt import SYSTEM_PROMPT
@@ -10,13 +10,22 @@ from prompt_library.prompt import SYSTEM_PROMPT
 
 class GraphBuilder():
 
-    def __init__(self):
-        self.tools = [
-            WeatherInfoTool(),
-            CalculatorTool(),
-            PlaceSearchTool(),
-            CurrencyConversionTool(),
-        ]
+    def __init__(self, model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm
+        self.tools = []
+        self.weather_info_tools = WeatherInfoTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_conversion_tools = CurrencyConversionTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.tools.extend([* self.weather_info_tools.tool_list,
+                           * self.calculator_tools.tool_list,
+                           * self.currency_conversion_tools.tool_list,
+                           * self.place_search_tools.tool_list])
+        self.llm_with_tools = self.llm.bind(tools=self.tools)
+
+        self.graph = None
+
 
     def agent_function(self, state: MessagesState):
         conversation = state['messages']
